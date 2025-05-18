29161ed32115051aac0dcba1bb92fd2ccaa808d0
MtMari <199931599+MtMari@users.noreply.github.com>
2025-05-18 12:32:25 +0200

    File completati e funzionanti

--- lezione-03/01-stanze/es-01-lz-03.php
+++ lezione-03/01-stanze/es-01-lz-03.php
@@ -3,9 +3,9 @@
     
     il PHP deve contenere un array delle stanze con i relativi occupanti e il numero di telefono interno; OK
     
-    quando l'utente manda il numero di stanza al PHP tramite il form, il PHP deve scrivere il nome dell'occupante e il relativo numero di telefono. 
+    quando l'utente manda il numero di stanza al PHP tramite il form, il PHP deve scrivere il nome dell'occupante e il relativo numero di telefono. OK
     
-    In caso non sia passato nessun numero di stanza il PHP deve scrivere un elenco di tutti i numeri di stanza con il relativo occupante e telefono.
+    In caso non sia passato nessun numero di stanza il PHP deve scrivere un elenco di tutti i numeri di stanza con il relativo occupante e telefono. OK
  */
 
 
@@ -35,12 +35,23 @@ $stanza = array(
     )
 );
 
-$occupante = $stanza[$_GET['stanza']];
 
-echo '<p> La stanza '
-    . $_GET['stanza']
-    . ' è occupata da '
-    . $stanza[$_GET['stanza']]['occupante']
-    . '. Digitare '
-    . $stanza[$_GET['stanza']]['telefono']
-    . ' per chiamare.';
+if (isset( $_POST['stanza'] )) {
+    echo '<p style="font-family: sans-serif;"> La stanza '
+        . $_POST['stanza']
+        . ' è occupata da '
+        . $stanza[ $_POST['stanza'] ]['occupante']
+        . '. <br> Digitare '
+        . $stanza[ $_POST['stanza'] ]['telefono']
+        . ' per chiamare.</p>';
+} else {
+    foreach ($stanza as $k => $v) {
+        echo '<p style="font-family: sans-serif;">La stanza '
+            . $k
+            . ' è occupata da '
+            . $v['occupante']
+            . '. Digitare '
+            . $v['telefono']
+            . ' per chiamare.</p>';
+    }
+}
